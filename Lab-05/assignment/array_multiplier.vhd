043761e7f596f2c83d300220b359335de3586bba
Ali Mohammadpour <aliemohammadpour@gmail.com>
2020-08-31 20:11:05 +0430

    feat: Add Lab05 Material and Template Sources

---
+++ Lab-05/assignment/array_multiplier.vhd
@@ -0,0 +1,39 @@
+--/*
+--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  Designed by Ali Mohammadpour(@alimpk)
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+-----------------------------------------------------------
+---  Module Name: Basic Multiplier 
+---  Description: Basic Multiplier 4 Bits
+-----------------------------------------------------------
+entity array_multiplier is
+	port (
+		A    : in  std_logic_vector(3 downto 0);
+		B    : in  std_logic_vector(3 downto 0);
+		P    : out std_logic_vector(7 downto 0)
+	);
+end array_multiplier;
+
+architecture array_multiplier_arc of array_multiplier is
+
+	-- add carry ripple  adder component here
+
+begin
+
+	-- write your code here!
+
+end array_multiplier_arc;
---
+++ Lab-05/assignment/half_adder.vhd
@@ -0,0 +1,37 @@
+--/*
+--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  Designed by Ali Mohammadpour(@alimpk)
+--  *******************************************************
+--  All Rights reserved (C) 2019-2020
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+-----------------------------------------------------------
+---  Module Name: Half Adder
+---  Description: Single Bit Half Adder
+-----------------------------------------------------------
+entity half_adder is
+	port (
+		a : in  std_logic;
+		b : in  std_logic;
+		s : out std_logic;
+		c : out std_logic
+	);
+end half_adder;
+
+architecture half_adder_arc of half_adder is
+begin
+
+	-- write your code here!
+
+end half_adder_arc;
